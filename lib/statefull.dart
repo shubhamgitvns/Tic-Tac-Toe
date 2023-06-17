714c842ca8329178faa2e21859b94e0d4ca87280
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-06-17 13:30:54 +0530

    Applay fix size in the chrome

--- lib/statefull.dart
+++ lib/statefull.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -13,55 +12,69 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var grid = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
 
-  var cuurenplayer='x';
-  void cross(i){
-    setState(() {
-      if(grid[i]=='-'){
-        grid[i] = cuurenplayer;
-        cuurenplayer = cuurenplayer ==  'x' ? 'o' : 'x';
-      }
+  var cuurenplayer = 'x';
+  void cross(i) {
+    setState(
+      () {
+        if (grid[i] == '-') {
+          grid[i] = cuurenplayer;
+          cuurenplayer = cuurenplayer == 'x' ? 'o' : 'x';
+        }
+        if(cuurenplayer == grid[3]){
+          print("hii");
 
-    });
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Tic Tac Toe "),
-          centerTitle: true,
-          shadowColor: Colors.tealAccent,
-        ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          color: Colors.black,
-          child: GridView.builder(
-            //if the background color using extra space than using shrinkwrap
-            // in this app using shrinkwrap in the black color
-            shrinkWrap: true,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 5,
-              mainAxisSpacing: 5,
+      appBar: AppBar(
+        title: const Text("Tic Tac Toe "),
+        centerTitle: true,
+        shadowColor: Colors.tealAccent,
+      ),
+      body: Column(
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxHeight: 500,
+              maxWidth: 500,
             ),
-            itemCount: grid.length,
-            itemBuilder: (context, index) => Material(
-              //Create the background color in the grid
-              color: Colors.blueAccent,
-              child: InkWell(
-                // if click the icon than work splash color like click button in the css
-                splashColor: Colors.teal,
-                onTap: (){
-                  cross(index);
-                },
-                child: Center(
-                    child: Text(
-                      grid[index],
-                      style: const TextStyle(fontSize: 50),
-                    )),
+            margin: const EdgeInsets.all(20),
+            color: Colors.black,
+            child: GridView.builder(
+              //if the background color using extra space than using shrinkwrap
+              // in this app using shrinkwrap in the black color
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+              ),
+              itemCount: grid.length,
+              itemBuilder: (context, index) => Material(
+                //Create the background color in the grid
+                color: Colors.blueAccent,
+                child: InkWell(
+                  // if click the icon than work splash color like click button in the css
+                  splashColor: Colors.teal,
+                  onTap: () {
+                    cross(index);
+                  },
+                  child: Center(
+                      child: Text(
+                    grid[index],
+                    style: const TextStyle(fontSize: 50),
+                  )),
+                ),
               ),
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
