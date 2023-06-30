ab5ba393174761d766475f4ac92efd460df1e4c2
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-06-30 12:34:26 +0530

    Choose option page

--- lib/stateless.dart
+++ lib/stateless.dart
@@ -23,18 +23,9 @@ class WelcomPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const SizedBox(height: 29),
                 const Text(
-                  'Welcome to',
-                  style: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    letterSpacing: 1.5,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'Tic Tac Toe',
+                  'Choose One Option',
                   style: TextStyle(
                     fontSize: 48,
                     fontWeight: FontWeight.bold,
@@ -42,9 +33,9 @@ class WelcomPage extends StatelessWidget {
                     letterSpacing: 1.5,
                   ),
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 29),
                 const Text(
-                  'With Computer',
+                  'Choose one option',
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
@@ -52,20 +43,100 @@ class WelcomPage extends StatelessWidget {
                     letterSpacing: 1.5,
                   ),
                 ),
-                const SizedBox(height: 32),
-                Container(
-                  width: 150,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(75),
-                  ),
-                  child: Center(
-                      child: SizedBox(
-                          height: 100,
-                          child: Image.network(
-                              'https://cdn.pixabay.com/photo/2013/07/12/15/56/tic-tac-toe-150614_1280.png'))),
+                //const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                            child: SizedBox(
+                                height: 100,
+                                child: Image.network(
+                                    'https://media.istockphoto.com/id/1097309742/vector/versus-screen-vs-battle-headline-conflict-duel-between-teams-confrontation-fight-competition.jpg?s=612x612&w=0&k=20&c=ZWrDXhVdD7TbqE_kzUVispiX5eN6tHDg0DvmVSUv8mg='))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                            child: SizedBox(
+                                height: 100,
+                                child: Image.network(
+                                    'https://media.istockphoto.com/id/937014004/vector/video-game-controller-icon-flat.jpg?s=612x612&w=0&k=20&c=Nxvg6dESxXCPIf29MlN8mwervkn7G2JwMbeTHP6RGAc='))),
+                      ),
+                    ),
+                  ],
                 ),
+
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   children: [
+                     Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: Center(
+                         child: ElevatedButton(
+                           onPressed: () {
+
+                           },
+                           style: ElevatedButton.styleFrom(
+                             primary: Colors.amberAccent,
+                             padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                             shape: RoundedRectangleBorder(
+                               borderRadius: BorderRadius.circular(30),
+                             ),
+                           ),
+                           child: const Text(
+                             'Human Mode',
+                             style: TextStyle(
+                                 fontSize: 24,
+                                 fontWeight: FontWeight.bold,
+                                 color: Colors.white
+                               // color: Color(0xFF09203f),
+                             ),
+                           ),
+                         ),
+                       ),
+                     ),
+                     Center(
+                       child: ElevatedButton(
+                         onPressed: () {
+                           // Navigate to the game screen
+
+                         },
+                         style: ElevatedButton.styleFrom(
+                           primary: Colors.amberAccent,
+                           padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                           shape: RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(30),
+                           ),
+                         ),
+                         child: const Text(
+                           'Computer mode',
+                           style: TextStyle(
+                               fontSize: 24,
+                               fontWeight: FontWeight.bold,
+                               color: Colors.white
+
+                           ),
+                         ),
+                       ),
+                     )
+                   ],
+                 ),
                  const SizedBox(height: 32),
                 const Text(
                   'Get ready to play!',
@@ -75,38 +146,7 @@ class WelcomPage extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ),
-                const SizedBox(height: 16),
-                const Text(
-                  'Challenge your opponent and be the champion.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to the game screen
-                    Navigator.pushNamed(context, '/game');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.amberAccent,
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: const Text(
-                    'Start Game',
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white
-                        // color: Color(0xFF09203f),
-                        ),
-                  ),
-                ),
+
               ],
             ),
           ),
